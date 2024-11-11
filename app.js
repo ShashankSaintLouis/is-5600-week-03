4af5e8147c4191953f854881adc5c55ee5909996
ShashankSaintLouis <thanugundlasindhu2001@gmail.com>
2024-11-11 04:52:14 +0000

    changes

--- app.js
+++ app.js
@@ -1,43 +1,13 @@
-const server = http.createServer(function(request, response) {
-  const parsedUrl = url.parse(request.url, true);
-  const pathname = parsedUrl.pathname;  
+const http = require('http');
+const url = require('url');
 
-  if (pathname === '/') return respondText(request, response);
-  if (pathname === '/json') return respondJson(request, response);
-  if (pathname.match(/^\/echo/)) return respondEcho(request, response);
-  
-  respondNotFound(request, response);
-});
 const port = process.env.PORT || 3000;
 
+// note that typically the variables here are `req` and `res` but we are using `request` and `response` for clarity
+const server = http.createServer(function(request, response) {
+  response.end("hi");
+});
 
-function respondText(req, res) {
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('hi');
-}
-
-function respondJson(req, res) {
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({ text: 'hi', numbers: [1, 2, 3] }));
-}
-
-function respondNotFound(req, res) {
-  res.writeHead(404, { 'Content-Type': 'text/plain' });
-  res.end('Not Found');
-}
-
-function respondEcho(req, res) {
-  const urlObj = new URL(req.url, `http://${req.headers.host}`);
-  const input = urlObj.searchParams.get('input') || '';
-
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({
-      normal: input,
-      shouty: input.toUpperCase(),
-      charCount: input.length,
-      backwards: input.split('').reverse().join(''),
-  }));
-}
 server.listen(port, function() {
   console.log(`Server is listening on port ${port}`);
 });
